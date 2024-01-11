2d8a84d86f7c8ce9c1d9a139ac4561b1effb0969
bacf <brunoagustincarusofassa@gmail.com>
2024-01-11 20:46:46 -0300

    added icons to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,13 +2,38 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
+const navIcons = [
+  { src: 'assets/icons/search.svg', alt: 'Search' },
+  { src: 'assets/icons/black-heart.svg', alt: 'Black Heart' },
+  { src: 'assets/icons/user.svg', alt: 'User' },
+];
+
 const Navbar = () => {
   return (
     <header className="w-full">
       <nav className="nav">
         <Link href="/" className="flex items-center gap-1">
-          <Image src="assets/icons/logo.svg" width={27} height={27} />
+          <Image
+            src="assets/icons/logo.svg"
+            width={27}
+            height={27}
+            alt="Logo"
+          />
+          <p className="nav-logo">
+            Price<span className="text-primary">Tracker</span>
+          </p>
         </Link>
+        <div className="flex items-center gap-5">
+          {navIcons.map((icon, index) => (
+            <Image
+              key={index}
+              src={icon.src}
+              width={28}
+              height={28}
+              alt={icon.alt}
+            />
+          ))}
+        </div>
       </nav>
     </header>
   );
