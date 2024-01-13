c15bf440a65fb8a8365eef4d354ad0ea52c2da83
bacf <brunoagustincarusofassa@gmail.com>
2024-01-12 22:48:27 -0300

    home section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,27 @@
+import Image from 'next/image';
 import React from 'react';
 
 const Home = () => {
-  return <div>home page</div>;
+  return (
+    <>
+      <section className="px-6 md:px-20 py-24 border-2 border-red-500">
+        <div className="flex max-xl:flex-col gap-16">
+          <div className="flex flex-col justify-center">
+            <p className="small-text">
+              Smart Shopping start here:{' '}
+              <Image
+                src="assets/icons/arrow-right.svg"
+                alt="Arrow Right"
+                width={16}
+                height={16}
+              />
+            </p>
+            <h1 className="head-text">Unleash the Power of tracking prices</h1>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Home;
