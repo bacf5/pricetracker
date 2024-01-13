6141ce461388d942cdd388cd0ded35c8952abedc
bacf <brunoagustincarusofassa@gmail.com>
2024-01-13 09:49:19 -0300

    Searchbar Style added

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+'use client';
+import React from 'react';
 
 const Searchbar = () => {
-  return (
-    <div>Searchbar</div>
-  )
-}
+  const handleSubmit = () => {};
 
-export default Searchbar
+  return (
+    <form className="flex flex-wrap gap-4 mt-12" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Enter product link"
+        className="searchbar-input"
+      />
+
+      <button type="submit" className="searchbar-btn">
+        Search
+      </button>
+    </form>
+  );
+};
+
+export default Searchbar;
