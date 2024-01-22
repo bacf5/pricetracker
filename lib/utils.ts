ce6fe14efea6a59e82329937301c057cb1c2eb9e
bacf <brunoagustincarusofassa@gmail.com>
2024-01-22 19:05:33 -0300

    some utils functions

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,6 @@
+import { PriceHistoryItem } from '@/types';
+import { cursorTo } from 'readline';
+
 export function makeStringToNumber(value: any) {
   const price = value[0];
   const priceNumber = parseFloat(price.replace(',', '.'));
@@ -10,4 +13,36 @@ export function makePercentagePrice(price: number, oldPrice: number) {
   const percentage = ((oldPrice - price) / oldPrice) * 100;
   const percentageRounded = Math.round(percentage);
   return percentageRounded;
+}
+
+export function getHighestPrice(priceList: PriceHistoryItem[]) {
+  let highestPrice = priceList[0];
+
+  for (let i = 0; i < priceList.length; i++) {
+    if (priceList[i].price > highestPrice.price) {
+      highestPrice = priceList[i];
+    }
+  }
+  return highestPrice.price;
+}
+
+export function getLowestPrice(priceList: PriceHistoryItem[]) {
+  let lowestPrice = priceList[0];
+
+  for (let i = 0; i < priceList.length; i++) {
+    if (priceList[i].price > lowestPrice.price) {
+      lowestPrice = priceList[i];
+    }
+  }
+  return lowestPrice.price;
+}
+
+export function getAveragePrice(priceList: PriceHistoryItem[]) {
+  const sumOfPrices = priceList.reduce(
+    (accum, currentValue) => accum + currentValue.price,
+    0
+  );
+  const avgPrice = sumOfPrices / priceList.length || 0;
+
+  return avgPrice;
 }
