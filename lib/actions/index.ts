7abee4884357b2cf521f99651a73222a38f4d4d2
bacf <brunoagustincarusofassa@gmail.com>
2024-01-22 19:00:37 -0300

    added getHighest/getLowest/getAverage prices & priceHistory[] type

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -3,6 +3,8 @@
 import Product from '../models/product.model';
 import { connectToDatabase } from '../mongoose';
 import { scrapePadelProduct } from '../scraper';
+import { getHighestPrice, getLowestPrice, getAveragePrice } from '../utils';
+import { revalidatePath } from 'next/cache';
 
 export async function scrapeAndStoreProduct(productUrl: string) {
   if (!productUrl) return;
@@ -20,7 +22,27 @@ export async function scrapeAndStoreProduct(productUrl: string) {
 
     // Create or update the product price history based on the scraped product
     if (existingProduct) {
+      const updatedPriceHistory: any = [
+        ...existingProduct.priceHistory,
+        {
+          price: scrapedProduct.finalPrice,
+        },
+      ];
+      product = {
+        ...scrapedProduct,
+        priceHistory: updatedPriceHistory,
+        lowestPrice: getLowestPrice(updatedPriceHistory),
+        highestPrice: getHighestPrice(updatedPriceHistory),
+        averagePrice: getAveragePrice(updatedPriceHistory),
+      };
     }
+
+    const newProduct = await Product.findOneAndUpdate(
+      { url: scrapedProduct.url },
+      product,
+      { upsert: true, new: true }
+    );
+    revalidatePath(`/products/${newProduct.id}`);
   } catch (error: any) {
     throw new Error(`Failed to create/update the product: ${error.message}`);
   }
