84b205ac07186f509fa176396b0a7510b43a19cb
bacf <brunoagustincarusofassa@gmail.com>
2024-01-24 19:15:55 -0300

    product page styling and added PriceInfoCard component to show more info about prices

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,3 +1,4 @@
+import { PriceInfoCard } from '@/components/PriceInfoCard';
 import { getProductById } from '@/lib/actions';
 import { Product } from '@/types';
 import Image from 'next/image';
@@ -58,7 +59,7 @@ const ProductDetails = async ({ params: { id } }: Props) => {
                   />
                   {/* look up for an svg about stock or not? and then a ternary op
                    */}
-                  <p className="text-sm font-semibold text-[#D46F77]">1</p>
+                  <p className="text-sm font-semibold text-[#D46F77]">37</p>
                 </div>
                 <div className="p-2 bg-white-200 rounded-10">
                   <Image
@@ -76,6 +77,56 @@ const ProductDetails = async ({ params: { id } }: Props) => {
                     height={20}
                   />
                 </div>
+              </div>
+            </div>
+            <div className="product-info">
+              <div className="flex flex-col gap-2">
+                <p className="text-[34px] text-secondary font-bold">
+                  € {product.finalPrice}
+                </p>
+                <p className="text-[21px] text-black opacity-50 line-through ">
+                  € {product.oldPrice}
+                </p>
+              </div>
+              <div className="flex flex-col gap-4">
+                <div className="flex gap-3">
+                  <div className="product-stars">
+                    <Image
+                      src="/assets/icons/star.svg"
+                      alt="star"
+                      width={16}
+                      height={16}
+                    />
+                    <p className="text-[12px] text-primary font-semibold">12</p>
+                  </div>
+                  <div className="product-reviews">
+                    <Image
+                      src="/assets/icons/comment.svg"
+                      alt="comments"
+                      height={16}
+                      width={16}
+                    />
+                    <p className="text-[12px] text-secondary font-semibold">
+                      18 Reviews
+                    </p>
+                  </div>
+                </div>
+
+                <p className="text-sm text-black opacity-50">
+                  <span className="text-primary-green font-semibold">90%</span>{' '}
+                  de compradores recomiendan este producto!
+                </p>
+              </div>
+            </div>
+
+            <div className="my-7 flex flex-col gap-5">
+              <div className="flex flex-wrap gap-5">
+                <PriceInfoCard
+                  title="Current Price"
+                  iconSrc="/assets/icons/price-tag.svg"
+                  value={`€ ${product.finalPrice}`}
+                  borderColor="#B6DBFF"
+                />
               </div>
             </div>
           </div>
