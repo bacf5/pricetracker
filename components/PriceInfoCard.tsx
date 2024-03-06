2ee62260a4b455a60706146424ce4922d5b72f12
bacf <brunoagustincarusofassa@gmail.com>
2024-03-05 23:46:16 -0300

    Commented discout, still working on it

--- components/PriceInfoCard.tsx
+++ components/PriceInfoCard.tsx
@@ -9,6 +9,9 @@ interface Props {
 export const PriceInfoCard = ({ title, iconSrc, value }: Props) => {
   return (
     <div className="price-info_card">
+      {/* <div className="absolute transform rotate-45 bg-green-500 text-center text-white font-semibold text-sm right-[-35px] top-[32px] w-[170px]">
+        ??% Descuento
+      </div> */}
       <p className="text-base text-black-100">{title}</p>
       <div className="flex gap-1">
         <Image src={iconSrc} alt={title} width={24} height={24} />
@@ -17,3 +20,5 @@ export const PriceInfoCard = ({ title, iconSrc, value }: Props) => {
     </div>
   );
 };
+
+// absolute transform rotate-45 bg-green-600 text-center text-white font-semibold py-1 right-[-35px] top-[32px] w-[170px]
